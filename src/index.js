a20772116bf8e8ba9a778c22a2b51fcb751307e8
KosmicDaKerbal <ankurparchure@gmail.com>
2024-08-19 12:01:36 +0530

    removed loggin

--- src/index.js
+++ src/index.js
@@ -59,12 +59,10 @@ client.on('interactionCreate', async (interact) => {
                 collector.on('collect', async (inter) => {
                     switch (inter.customId){
                         case 'confirm':
-                            console.log('confirmed');
                             confirmbox.setDescription("Linked Account: " + String(interact.options.get('account-name').value) + " Successfully.").setColor (0x0000FF).setTimestamp();
                             await interact.editReply({embeds: [confirmbox]});
                             break;
                         case 'cancel':
-                            console.log('cancelled');
                             confirmbox.setDescription("Cancelled Linking Account: " + String(interact.options.get('account-name').value)).setColor (0xFF0000).setTimestamp();
                             await interact.editReply({embeds: [confirmbox]});
                         break;
