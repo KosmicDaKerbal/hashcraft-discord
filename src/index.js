fa5edbec7cf1553a2c102ec198336e4151c2b7b9
KosmicDaKerbal <ankurparchure@gmail.com>
2024-08-24 18:12:59 +0530

    5

--- src/index.js
+++ src/index.js
@@ -467,8 +467,9 @@ client.on("interactionCreate", async (mainInteraction) => {
               async function (err, result) {
               if (!err){
                 const streak = result[0].streak;
-                const use = result[0].last_used;
-                console.log (use);
+                const use = String(result[0].last_used).substring(0,9);
+                console.log(use);
+                console.log(dayjs(use));
                 if (dayjs(use).isYesterday){
                   if (streak <= 100){
                     const drop = Math.ceil(((streak * streak)/125)+10);
