57dab9f2b9578208030e3cc9d8e99232f60fc535
KosmicDaKerbal <ankurparchure@gmail.com>
2024-08-24 20:17:45 +0530

    v0.4.9 dry-run

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const ver = "0.4.9 unstable";
+const ver = "0.4.9 dry-run";
 const ico = "https://i.postimg.cc/dVvZgrNp/Hash-Craft-Logo.png";
 const loading = "https://media.tenor.com/-n8JvVIqBXkAAAAM/dddd.gif";
 const done = "https://i.postimg.cc/gjWcVZgy/green-check-mark-icon-in-round-shape-design-png.webp";
@@ -469,53 +469,62 @@ client.on("interactionCreate", async (mainInteraction) => {
           await mainInteraction.reply({ embeds: [claimbox] });
           const claimtime = dayjs();
           con.query(
-            `select streak, last_used from Faucet where userid = ${u};`,
+            `select wallet_name, streak, last_used from Faucet where userid = ${u};`,
             async function (err, result) {
               if (!err) {
-                var streak = result[0].streak;
-                const use = result[0].last_used;
-                const timediff = claimtime.diff(use, 'day')
-                switch (timediff) {
-                  case 0:
-                    claimbox.setAuthor(
-                      { name: 'HashCraft Faucet', iconURL: notdone }
-                    ).setTitle(`Don't be Greedy!`).setDescription(`You have claimed already. Try again tomorrow.`).setColor(0xff0000);
-                    await mainInteraction.editReply({ embeds: [claimbox] });
-                    break;
-                  default:
-                    var lost;
-                    if (timediff == 1){
-                      streak = streak + 1;
-                      lost = 0;
-                    } else {
-                      lost = 1;
-                    }
-                    var drop;
-                    if (streak <= 100) {
-                      drop = Math.ceil(((streak * streak) / 111) + 10);
-                    } else {
-                      drop = 100;
-                    }
-                    con.query(
-                      `insert into Faucet (userid, last_used, streak) values (${u}, '${claimtime.format("YYYY-MM-DD")}', ${streak}) on duplicate key update mdu_bal = mdu_bal + ${drop}, claims = claims + 1, streak = ${streak}, last_used = '${claimtime.format("YYYY-MM-DD")}';`,
-                      async function (err, result) {
-                        if (!err) {
-                          claimbox.setAuthor(
-                            { name: 'HashCraft Faucet', iconURL: done }
-                          ).setTitle(`Claim Successful`).setDescription(`Drop: ⧈${drop}\nCurrent streak: ${streak}`).setColor(0x00ff00);
-                          if (lost && use != null){
-                           claimbox.setDescription(`Drop: ⧈${drop}\nYou lost your streak of ${streak}`);
-                           streak = 1;
+                if (result[0].wallet_name == null){
+                  claimbox.setAuthor(
+                    { name: 'HashCraft Faucet', iconURL: notdone }
+                  ).setTitle(`Account not linked yet`).setDescription(`You haven't linked your Duino-Coin Account to this discord user. Run /link to do so.`).setColor(0xff0000);
+                  await mainInteraction.editReply({ embeds: [claimbox] });
+                } else {
+                  var streak = result[0].streak;
+                  const use = result[0].last_used;
+                  const timediff = claimtime.diff(use, 'day')
+                  switch (timediff) {
+                    case 0:
+                      claimbox.setAuthor(
+                        { name: 'HashCraft Faucet', iconURL: notdone }
+                      ).setTitle(`Don't be Greedy!`).setDescription(`You have claimed already. Try again tomorrow.`).setColor(0xff0000);
+                      await mainInteraction.editReply({ embeds: [claimbox] });
+                      break;
+                    default:
+                      var lost;
+                      if (timediff == 1){
+                        streak = streak + 1;
+                        lost = 0;
+                      } else {
+                        lost = 1;
+                      }
+                      var drop;
+                      if (streak <= 100) {
+                        drop = Math.ceil(((streak * streak) / 111) + 10);
+                      } else {
+                        drop = 100;
+                      }
+                      con.query(
+                        `insert into Faucet (userid, last_used, streak) values (${u}, '${claimtime.format("YYYY-MM-DD")}', ${streak}) on duplicate key update mdu_bal = mdu_bal + ${drop}, claims = claims + 1, streak = ${streak}, last_used = '${claimtime.format("YYYY-MM-DD")}';`,
+                        async function (err, result) {
+                          if (!err) {
+                            claimbox.setAuthor(
+                              { name: 'HashCraft Faucet', iconURL: done }
+                            ).setTitle(`Claim Successful`).setDescription(`Drop: ⧈${drop}\nCurrent streak: ${streak}`).setColor(0x00ff00);
+                            if (lost && use != null){
+                             claimbox.setDescription(`Drop: ⧈${drop}\nYou lost your streak of ${streak}`);
+                             streak = 1;
+                            } else {
+                              streak = 1;
+                            }
+                            await mainInteraction.editReply({ embeds: [claimbox] });
+                          } else {
+                            claimbox.setAuthor(
+                              { name: 'HashCraft Faucet', iconURL: notdone }
+                            ).setTitle(`Error`).setDescription(`Could not process query`).setColor(0xff0000);
+                            await mainInteraction.editReply({ embeds: [claimbox] });
                           }
-                          await mainInteraction.editReply({ embeds: [claimbox] });
-                        } else {
-                          claimbox.setAuthor(
-                            { name: 'HashCraft Faucet', iconURL: notdone }
-                          ).setTitle(`Error`).setDescription(`Could not process query`).setColor(0xff0000);
-                          await mainInteraction.editReply({ embeds: [claimbox] });
-                        }
-                      });
-                break;
+                        });
+                  break;
+                  }
                 }
               } else {
                 claimbox.setAuthor(
