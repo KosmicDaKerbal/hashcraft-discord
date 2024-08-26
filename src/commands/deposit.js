6f3c41e0e115e31b41b58398812527917ff7f41a
KosmicDaKerbal <ankurparchure@gmail.com>
2024-08-26 16:08:52 +0530

    self host

--- src/commands/deposit.js
+++ src/commands/deposit.js
@@ -42,7 +42,7 @@ module.exports = {
                       `update Faucet set mdu_bal = ${bal-dep} where Faucet.userid = ${userid}`,
                       async function (err) {
                         if (!err){
-                          const url = `https://server.duinocoin.com/transaction/?username=` + encodeURIComponent(process.env.MASTER_USER) + `&password=` + encodeURIComponent(process.env.MASTER_KEY) + `&recipient=` + encodeURIComponent(recip) + `&amount=` + encodeURIComponent(send) + `&memo=HashCraft_Faucet`;
+                          const url = `http://server.duinocoin.com/transaction/?username=` + encodeURIComponent(process.env.MASTER_USER) + `&password=` + encodeURIComponent(process.env.MASTER_KEY) + `&recipient=` + encodeURIComponent(recip) + `&amount=` + encodeURIComponent(send) + `&memo=HashCraft_Faucet`;
                           http.get(url,(res) => {
                             let data = "";
                             res.on("data", (chunk) => {
