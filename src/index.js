e9e9e30611ff11174334d4c9679a2d7f4e9c3715
KosmicDaKerbal <ankurparchure@gmail.com>
2024-08-28 23:35:13 +0530

    use root password

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const balance = require("./commands/balance");
 var con = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT
 });
