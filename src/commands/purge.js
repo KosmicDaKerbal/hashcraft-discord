2cf52dfc6a7eb8d1dfcb9da3c4fbe2d36e328644
KosmicDaKerbal <ankurparchure@gmail.com>
2024-09-12 09:28:24 +0530

    6

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -3,16 +3,16 @@ const { EmbedBuilder } = require("discord.js");
 module.exports = {
     execute: async function (embed) {
         const purge = new EmbedBuilder().setFooter({ text: `${process.env.BOT_NAME} v${process.env.BOT_VERSION}`, iconURL: process.env.ICON }).setTimestamp();
-        if (embed.options.get("purge-limit").value > 0) {
+        const param = Math.abs(embed.options.getInteger("purge-limit").value);
+        if (100 - param > 0) {
             purge.setAuthor({ name: `${process.env.BOT_NAME} Administration`, iconURL: process.env.SUCCESS })
-            .setTitle("Purged \`" + embed.options.get("purge-limit").value + "\` Messages.")
+            .setTitle("Purged \`" + param + "\` Messages.")
             .setColor(0x00ff00);
-            let msg = embed.options.getInteger("purge-limit")
-            embed.channel.bulkDelete(msg);
+            await embed.channel.bulkDelete(param);
             await embed.reply({ embeds: [purge] });
         } else {
             purge.setAuthor({ name: `${process.env.BOT_NAME} Administration`, iconURL: process.env.FAIL })
-            .setTitle("Value must be greater than 0")
+            .setTitle("Value must be greater than 0 and less than 100.")
             .setColor(0xff0000);
             await embed.reply({ embeds: [purge] });
         }
