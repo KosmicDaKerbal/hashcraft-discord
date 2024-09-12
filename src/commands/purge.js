9c5ff81ce81e651b3bd096bebd09c07bfc94e4a4
KosmicDaKerbal <ankurparchure@gmail.com>
2024-09-12 08:47:21 +0530

    2

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -8,7 +8,7 @@ module.exports = {
             .setTitle("Purged \`" + embed.options.get("purge-limit").value + "\` Messages.")
             .setColor(0x00ff00);
             let messagecount = parseInt(embed.options.get("purge-limit"));
-            embed.channel.messages.fetch({ limit: messagecount }).then(async messages => await embed.channel.bulkDelete(messages))
+            embed.channel.messages.fetch({ limit: messagecount }).then(messages => embed.channel.bulkDelete(messages))
             .catch(err =>
             purge.setAuthor({ name: `${process.env.BOT_NAME} Administration`, iconURL: process.env.FAIL })
             .setTitle("Error: Operation cancelled.")
