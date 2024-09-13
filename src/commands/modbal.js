f49fffc47149c7b3bae0c0309faabfce102ddbd2
KosmicDaKerbal <ankurparchure@gmail.com>
2024-09-13 08:39:44 +0530

    f

--- src/commands/modbal.js
+++ src/commands/modbal.js
@@ -3,7 +3,8 @@ const { EmbedBuilder } = require("discord.js");
 module.exports = {
     modify: async function (embed, con) {
     const modbal = new EmbedBuilder().setFooter({ text: `${process.env.BOT_NAME} v${process.env.BOT_VERSION}`, iconURL: process.env.ICON }).setTimestamp();
-    console.log(embed.options.get("purge-limit").value);
+    console.log(embed.options.get("user-to-modify").value);
+    console.log(embed.options.get("new-balance").value);
     embed.reply("check logs");
     }
 }
