84d7aa1c62c32fe3ad6309ea1c7530a2a3b780be
KosmicDaKerbal <ankurparchure@gmail.com>
2024-09-13 08:18:32 +0530

    error handling

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -7,7 +7,14 @@ module.exports = {
             purge.setAuthor({ name: `${process.env.BOT_NAME} Administration`, iconURL: process.env.SUCCESS })
             .setTitle("Purged \`" + param + "\` Messages.")
             .setColor(0x00ff00);
+            try {
             await embed.channel.bulkDelete(param);
+            } catch (err){
+            purge.setAuthor({ name: `${process.env.BOT_NAME} Administration`, iconURL: process.env.FAIL })
+            .setTitle("Purge Failed")
+            .setColor(0xff0000)
+            .setDescription("Error Log:\n\`\`\`\n" + err + "\n\`\`\`");
+            }
             await embed.reply({ embeds: [purge] });
     }
 }
