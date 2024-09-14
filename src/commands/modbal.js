f966d68a916b766a9a1be57d17ce333ba7e17236
KosmicDaKerbal <ankurparchure@gmail.com>
2024-09-14 20:34:59 +0530

    modbal implementation

--- src/commands/modbal.js
+++ src/commands/modbal.js
@@ -6,5 +6,27 @@ module.exports = {
     const uid = embed.options.get("user-to-modify").value;
     const nbal = embed.options.get("new-balance").value;
     await embed.deferReply();
+    con.getConnection(async function (err, conmodbal) {
+        if (err) {
+          modbal.setAuthor({ name: process.env.BOT_NAME + ' Administration', iconURL: process.env.FAIL })
+            .setTitle("Error: Unable to connect to DB.").setDescription("Log: \n\`\`\`\n" + err + "\n\`\`\`\nPlease try again.").setColor(0xff0000);
+          await embed.followUp({ embeds: [modbal] });
+        } else {
+          conmodbal.query(
+            `update Faucet set mdu_bal = ${nbal} where Faucet.userid = ${uid}`,
+            async function (err) {
+              if (!err) {
+                modbal.setAuthor({ name: process.env.BOT_NAME + ' Administration', iconURL: process.env.SUCCESS })
+            .setTitle("Modified balance of <@" + uid + ">").setDescription(`Set user's ⧈ mDU Balance to ⧈${nbal}`).setColor(0x00ff00);
+          await embed.followUp({ embeds: [modbal] });
+              } else {
+                modbal.setAuthor({ name: process.env.BOT_NAME + ' Administration', iconURL: process.env.FAIL })
+            .setTitle("Error: Query Failed.").setDescription("Log: \n\`\`\`\n" + err + "\n\`\`\`\nPlease try again.").setColor(0xff0000);
+          await embed.followUp({ embeds: [modbal] });
+              }
+            });
+    }
+    conmodbal.release();
+    });
     }
 }
