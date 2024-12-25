66076c1998e899c02978292be78f1ab9a445efd1
KosmicDaKerbal <ankurparchure@gmail.com>
2024-12-25 07:36:52 +0530

    set global application commands

--- src/slashRegister.js
+++ src/slashRegister.js
@@ -127,10 +127,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 (async () => {
     try {
         console.log('Registering Slash Commands...');
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body: commands }
-        );
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
         console.log('Slash Commands Registration Successful.');
     } catch (error) {
         console.log(`Error: ${error}`);
