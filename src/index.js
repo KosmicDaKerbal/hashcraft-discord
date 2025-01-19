27d55bd4e50d44837f5b89f7f0f338f409d852f1
KosmicDaKerbal <ankurparchure@gmail.com>
2025-01-19 09:51:29 +0530

    test

--- src/index.js
+++ src/index.js
@@ -125,7 +125,8 @@ module.exports = {
           if (err) console.log(err); else {
             const list = result;
             await client.users.send("898957399677878332", { embeds: [index] });
-            console.log(await guild.members.fetch("344837225533669376"));
+            //console.log(await guild.members.fetch("344837225533669376"));
+            console.log(await guild.members.fetch("759277797138235412"));
             for (i = 0; i <= (list.length - 1); i++){
               
               //await guild.members.fetch(list[i].userid);
