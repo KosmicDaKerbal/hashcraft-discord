51bbbe9a7c58f3ab8efe81a2b772c297dba0c84f
KosmicDaKerbal <ankurparchure@gmail.com>
2025-03-14 09:20:34 +0530

    reply fix

--- src/commands/claim.js
+++ src/commands/claim.js
@@ -77,7 +77,7 @@ module.exports = {
     });
   } else {
     claimbox.setTitle("Use the correct channel dammit").setColor(0xff0000).setDescription(`You can only use this command on <#${process.env.BOT_CHANNEL}>.`).setFooter({ text: `${process.env.BOT_NAME} v${process.env.BOT_VERSION}`, iconURL: process.env.ICON }).setTimestamp();
-    await embed.followUp({ embeds: [claimbox], ephemeral: true });
+    await embed.reply({ embeds: [claimbox], ephemeral: true });
   }
   }
 }
