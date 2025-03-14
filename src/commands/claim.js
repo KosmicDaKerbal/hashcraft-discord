cf585d53288ab7e6cd287f2a5c736a5a124d7b77
KosmicDaKerbal <ankurparchure@gmail.com>
2025-03-14 09:03:26 +0530

    3

--- src/commands/claim.js
+++ src/commands/claim.js
@@ -33,10 +33,9 @@ module.exports = {
                       const timediff = claimtime.diff(use, 'day');
                       switch (timediff) {
                         case 0:
-                          const cool = dayjs();
-                          const cooldown = cool.add(1, 'day');
-                          cooldown.set('hour', 0).set('minute', 0).set('second', 0);
-                          claimbox.setAuthor({ name: `${process.env.BOT_NAME} Faucet`, iconURL: process.env.FAIL }).setTitle(`Don't be Greedy!`).setDescription(`You have claimed already. Try again <t:${cooldown.unix()}:R>`).setColor(0xff0000);
+                          //const cool = dayjs();
+                          const cooldown = claimtime.add(1, 'day');
+                          claimbox.setAuthor({ name: `${process.env.BOT_NAME} Faucet`, iconURL: process.env.FAIL }).setTitle(`Don't be Greedy!`).setDescription(`You have claimed already. Try again <t:${cooldown.unix().set('hour', 0).set('minute', 0).set('second', 0)}:R>`).setColor(0xff0000);
                           await embed.followUp({ embeds: [claimbox] });
                           break;
                         default:
